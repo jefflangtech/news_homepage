3bec4361db469ce48108e19a0aeb8f0462553c15
jefflangtech <jeff.m.lang@gmail.com>
2023-03-05 17:22:53 -0800

    Fixed and created functionality for the slide-out nav, also fixed the feature/new grid areas

---
+++ resources/js/index.js
@@ -0,0 +1,24 @@
+const menuOpenButton = document.getElementById('menu-open-button');
+const menuCloseButton = document.getElementById('menu-close-button');
+const overlay = document.getElementById('overlay');
+const navMenu = document.getElementById('nav-menu');
+
+const toggleVisibility = function(element) {
+  element.classList.toggle('hidden');
+}
+
+const toggleNavMenu = function() {
+  toggleVisibility(menuOpenButton);
+  toggleVisibility(menuCloseButton);
+  toggleVisibility(overlay);
+
+  if(navMenu.getAttribute('data-attribute') === 'false') {
+    navMenu.setAttribute('data-attribute', 'true');
+  }
+  else {
+    navMenu.setAttribute('data-attribute', 'false');
+  }
+}
+
+menuOpenButton.addEventListener('click', toggleNavMenu, false);
+menuCloseButton.addEventListener('click', toggleNavMenu, false);
